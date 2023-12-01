f40185a53f9787f7cc464210637c1522dd7ab119
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-01 16:56:19 -0600

    Flush out the README.md

--- README.md
+++ README.md
@@ -1,24 +1,75 @@
-# README
+# Tea Time
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
 
-Things you may want to cover:
+## Initial Development Dates
+- November 24, 2023 - November 25, 2023
 
-* Ruby version
+## Built With
+- Ruby 3.2.2
+- Rails 7.0.8
 
-* System dependencies
+## Testing Tools
+- RSpec 3.12
+- Capybara 
+- Webmock 
+- VCR 
+- Pry
+- Simplecov
+- Shoulda-Matchers
+- Factory Bot Rails
+- Faker
 
-* Configuration
+## Schema Visualization
 
-* Database creation
+![Screenshot 2023-12-01 at 4 54 17 PM](https://gist.github.com/assets/132889569/5c28276c-02f1-403b-ba34-978f71f29d66)
 
-* Database initialization
 
-* How to run the test suite
 
-* Services (job queues, cache servers, search engines, etc.)
+### Getting Started:
+* Fork the repository to your own account.
+* Clone down to your local machine using your SSH key and running the command `git clone [input SSH Key here]` and pressing enter.
 
-* Deployment instructions
 
-* ...
+## API Documentation 
+
+- **POST /api/v1/subscriptions**: Create a new subscription.
+- Example Request Call:
+```
+POST /api/v1/subscriptions
+
+Content-Type: application/json
+Accept: application/json
+```
+- Example Response Call:
+```
+
+```
+
+- **GET /api/v1/subscriptions**: Retrieve a list of subscriptions.
+- Example Request Call:
+```
+GET /api/v1/subscriptions
+
+Content-Type: application/json
+Accept: application/json
+```
+- Example Response Call:
+```
+
+```
+
+- **DELETE /api/v1/subscriptions/:id**: Deletes a subscription.
+- Example Request Call:
+```
+DELETE /api/v1/subscriptions/1
+
+Content-Type: application/json
+Accept: application/json
+```
+- Example Response Call:
+```
+
+```
+
+## Contributors 
+- Ethan Van Gorkom | [GitHub](https://github.com/EVanGorkom) | [LinkedIn](https://www.linkedin.com/in/evangorkom/)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+customer1 = Customer.create(first_name: "Bob", last_name: "Dude", email: "123@gmail.com", address: "sesame street")
+customer2 = Customer.create(first_name: "Sally", last_name: "Dude", email: "321@gmail.com", address: "sesame street")
+customer3 = Customer.create(first_name: "Alex", last_name: "Dude", email: "abc@gmail.com", address: "sesame street")
+
+bob_sub1 = Subscription.create(title: "Bob's Sub 1", price: 12, frequency: 3, customer_id: customer1.id)
+bob_sub2 = Subscription.create(title: "Bob's Sub 2", price: 20, frequency: 3, customer_id: customer1.id)
+
+sally_sub1 = Subscription.create(title: "Sally's Sub 1", price: 12, frequency: 3, customer_id: customer2.id)
+sally_sub2 = Subscription.create(title: "Sally's Sub 2", price: 20, frequency: 3, customer_id: customer2.id)
+
+alex_sub1 = Subscription.create(title: "Alex's Sub 1", price: 12, frequency: 3, customer_id: customer3.id)
+alex_sub2 = Subscription.create(title: "Alex's Sub 2", price: 20, frequency: 3, customer_id: customer3.id)
+
+tea1 = Tea.create(title: "Ghyshal Greens", description: "A nice herbal tea", temperature: 105, brew_time: 5)
+tea2 = Tea.create(title: "Potion", description: "A basic black tea", temperature: 105, brew_time: 5)
+tea3 = Tea.create(title: "Hi-Potion", description: "A relaxing tea", temperature: 105, brew_time: 5)
+tea4 = Tea.create(title: "Elixir", description: "A refreshing balmy tea", temperature: 105, brew_time: 5)
+tea5 = Tea.create(title: "Pheonix Down", description: "A restorative tea", temperature: 105, brew_time: 5)
+
+TeaSubscription.create(subscription_id: bob_sub1.id, tea_id: tea1.id)
+TeaSubscription.create(subscription_id: bob_sub1.id, tea_id: tea2.id)
+TeaSubscription.create(subscription_id: bob_sub1.id, tea_id: tea3.id)
+TeaSubscription.create(subscription_id: bob_sub2.id, tea_id: tea4.id)
+TeaSubscription.create(subscription_id: bob_sub2.id, tea_id: tea5.id)
+
+TeaSubscription.create(subscription_id: sally_sub1.id, tea_id: tea1.id)
+TeaSubscription.create(subscription_id: sally_sub1.id, tea_id: tea2.id)
+TeaSubscription.create(subscription_id: sally_sub1.id, tea_id: tea3.id)
+TeaSubscription.create(subscription_id: sally_sub2.id, tea_id: tea3.id)
+TeaSubscription.create(subscription_id: sally_sub2.id, tea_id: tea5.id)
+
+TeaSubscription.create(subscription_id: alex_sub1.id, tea_id: tea1.id)
+TeaSubscription.create(subscription_id: alex_sub1.id, tea_id: tea2.id)
+TeaSubscription.create(subscription_id: alex_sub1.id, tea_id: tea3.id)
+TeaSubscription.create(subscription_id: alex_sub2.id, tea_id: tea5.id)
+TeaSubscription.create(subscription_id: alex_sub2.id, tea_id: tea1.id)
